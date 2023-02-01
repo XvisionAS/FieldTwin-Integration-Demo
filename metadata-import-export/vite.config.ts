635cb071328e2092bf94fcaf395444d2e3968380
Kr1s-z <kris.z@futureon.com>
2023-02-01 16:45:03 +0000

    INTE-503 Added selecting object

--- metadata-import-export/vite.config.ts
+++ metadata-import-export/vite.config.ts
@@ -1,8 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
+import mkcert from 'vite-plugin-mkcert'
 
 const config: UserConfig = {
-	plugins: [sveltekit()]
+	server: { https: true },
+	plugins: [sveltekit(), mkcert()]
 };
 
 export default config;
