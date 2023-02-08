f13bcb346365c4bfb401b8c728069c8fe3d007bf
Matt Fozard <matt@futureon.com>
2023-02-08 10:21:01 +0000

    feat: update for API 1.9, simplify, tidies

--- scripts/javascript/create-layout/env.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-export TOKEN="token"
-export LEGACY_API_HOST="legacyapi.[instance].fieldap.com"
-export PROJECT_ID="-projectId"
-export SUB_PROJECT_ID="subProjectId"
--- scripts/javascript/create-layout/get-layout.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-#!/usr/bin/env bash
-
-node get-layout.js --project=$PROJECT_ID --sub-project=$SUB_PROJECT_ID
