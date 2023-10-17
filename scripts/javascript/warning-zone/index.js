d176ac9628c693da92c91bd098dfff373e1b4b45
Ferdous Jahan <ferdousjahan2@gmail.com>
2023-10-17 10:43:59 +0200

    functions changed to async

--- scripts/javascript/warning-zone/index.js
+++ scripts/javascript/warning-zone/index.js
@@ -3,7 +3,7 @@ const parseArgs = require('minimist')
 
 const TOKEN = process.env.TOKEN || '91f8153e-ab28-43f6-b389-a6af824f8fcc'
 const PORT = process.env.PORT || ''
-const BACKEND_HOST = `${process.env.BACKEND_HOST ? 'https://'+process.env.BACKEND_HOST : 'http://futureon-backend.lvh.me'}`
+const BACKEND_HOST = `${process.env.BACKEND_HOST ? 'https://' + process.env.BACKEND_HOST : 'http://futureon-backend.lvh.me'}`
 const HOST_URL = `${BACKEND_HOST}${PORT ? ':' : ''}${PORT}`
 const API_VERSION = 'v1.9'
 
@@ -15,42 +15,37 @@ const STREAM_ID = argv['stream']
 const stagedAssetsUrl = `${HOST_URL}/API/${API_VERSION}/${PROJECT_ID}/subProject/${SUB_PROJECT_ID}:${STREAM_ID}/stagedassets`
 const shapesUrl = `${HOST_URL}/API/${API_VERSION}/${PROJECT_ID}/subProject/${SUB_PROJECT_ID}:${STREAM_ID}/shapes`
 
-let stagedAssets;
-let shapes;
+const main = async () => {
+    const stagedAssetsReq = await axios.get(stagedAssetsUrl, { headers: { token: TOKEN } })
+    const stagedAssets = stagedAssetsReq.data
 
-axios.get(stagedAssetsUrl, {
-    headers: { token: TOKEN }
-}).then(res => {
-    stagedAssets = res.data;
+    const shapesReq = await axios.get(shapesUrl, { headers: { token: TOKEN } })
+    const shapes = shapesReq.data.shapes
 
-    axios.get(shapesUrl, {
-        headers: { token: TOKEN }
-    }).then(res => {
-        shapes = res.data.shapes;
-        if (stagedAssets && shapes) {
-            for (const shape of shapes) {
-                if (shape.shapeType === 'Circle') {
-                    const stagedAssetsInOverlappedZone = isInside(shape, stagedAssets);
-                    if (stagedAssetsInOverlappedZone.length > 0) {
-                        addWarningTag(stagedAssetsInOverlappedZone)
-                    }
+    if (stagedAssets && shapes) {
+        for (const shape of shapes) {
+            if (shape.shapeType === 'Circle') {
+                const stagedAssetsInOverlappedZone = await isInside(shape, stagedAssets)
+                if (stagedAssetsInOverlappedZone.length > 0) {
+                    const patchedShapes = await addWarningTag(stagedAssetsInOverlappedZone)
+                    console.log(patchedShapes)
                 }
             }
         }
-    }).catch(error => console.log(error))
-}).catch(error => console.log(error))
+    }
+}
 
-const isInside = (shape, stagedAssets) => {
-    const inDangerZoneAsstes = [];
+const isInside = async (shape, stagedAssets) => {
+    const inDangerZoneAsstes = []
     for (const key in stagedAssets) {
         if ((stagedAssets[key].initialState.x - shape.x) * (stagedAssets[key].initialState.x - shape.x) +
             (stagedAssets[key].initialState.y - shape.y) * (stagedAssets[key].initialState.y - shape.y) <= shape.circleRadius * shape.circleRadius)
-            inDangerZoneAsstes.push(stagedAssets[key]);
+            inDangerZoneAsstes.push(stagedAssets[key])
     }
-    return inDangerZoneAsstes;
+    return inDangerZoneAsstes
 }
 
-const addWarningTag = (stagedAssetsInOverlappedZone) => {
+const addWarningTag = async (stagedAssetsInOverlappedZone) => {
     const payload = {
         "globals": {
             "tags": ["warning!!"]
@@ -60,12 +55,23 @@ const addWarningTag = (stagedAssetsInOverlappedZone) => {
         payload[stagedAsset.id] = {}
     }
 
-    axios.patch(stagedAssetsUrl,
+    const patchShapesReq = await axios.patch(stagedAssetsUrl,
         payload,
         { headers: { 'Content-Type': 'application/json', token: TOKEN }, }
-    ).then((response) => {
-        console.log(response)
-    }).catch((error) => {
-        console.log(error)
-    })
+    )
+    const patchedShapes = patchShapesReq.data
+
+    return patchedShapes
 }
+
+main().catch((e) => {
+    if (e.response) {
+        console.error(
+            `\nError:\n${e.request.method} ${e.request.path}\n` +
+            `${e.response.status} ${e.response.statusText}\n${JSON.stringify(e.response.data)}`
+        )
+    } else {
+        console.error(e)
+    }
+    process.exit(1)
+})
